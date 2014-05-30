50f2e1919cdee364af143fe427c5177e1c3104e2
Diego Medina <fmpwizard@gmail.com>
2014-05-30 17:07:19 -0400

    don't hardcode the hostname of ajax calls , assume same server

--- webapp/main.js
+++ webapp/main.js
@@ -1,6 +1,6 @@
 function moveRover(direction){
 	$.ajax({
-	  url: "http://cameraserver:7070/rover?c=" + direction + "&v=1",
+	  url: "/rover?c=" + direction + "&v=1",
 	  context: document.body
 	}).done(function() {
 	  alert("made it");
